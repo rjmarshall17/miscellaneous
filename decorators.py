0bb0cc76133fbb2a1a0f41fe2600c3a7f386f383
Rob Marshall <rob.marshall17@gmail.com>
2020-11-25 15:03:43 -0500

    Fix decorator example

--- decorators.py
+++ decorators.py
@@ -1,16 +1,35 @@
 #!/usr/bin/env python3
 
 from functools import wraps
+from random import randint
 
 
-@wraps
-def checker(func):
-    def inner(*args, **kwargs):
-        result = func(*args, **kwargs)
-        print("%s(%s) = %s" % (func.__name__,','.join(*args)),result)
-        return result
-    return inner
+def print_function(func):
+    # @wraps wraps the inner function in order to preserve the name of the
+    # wrapped/decorated function
+    
+	@wraps(func)
+	def inner(*args, **kwargs):
+		result = func(*args, **kwargs)
+        
+        # In order to join *args, you have to convert it to a list
+        # and make sure all of the elements are strings
+		print("%s(%s) = %s" % (func.__name__,
+                               ', '.join([str(x) for x in list(args)]),
+                               result))
+		return result
+	return inner
 
 
-def add(a,b):
-    return a + b
+def add(a, b, c, d):
+    return a + b + c + d
+
+if __name__ == '__main__':
+    add = print_function(add)
+    help(add)
+    a = randint(1, 30)
+    b = randint(1, 30)
+    c = randint(1, 30)
+    d = randint(1, 30)    
+    result = add(a, b, c, d)
+    print("The result from add(%d,%d,%d,%d) is: %d" % (a, b, c, d, result))
