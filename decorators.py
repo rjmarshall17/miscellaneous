b9d1d232c7f86cdc6e1043d87070a95af3ebf097
Rob Marshall <rob.marshall17@gmail.com>
2020-12-04 11:02:42 -0500

    Some fixes and tweaks

--- decorators.py
+++ decorators.py
@@ -3,14 +3,26 @@
 from functools import wraps
 
 
-@wraps
 def checker(func):
+    @wraps(func)
     def inner(*args, **kwargs):
-        result = func(*args, **kwargs)
-        print("%s(%s) = %s" % (func.__name__,','.join(*args)),result)
-        return result
+        all_args = ''
+        wrapped_result = func(*args, **kwargs)
+        all_args = ','.join([str(x) for x in args])
+        if len(kwargs) > 0:
+            if len(args) > 0:
+                all_args += ","
+            all_args += ','.join(["%s=%s" % (k, v) for k, v in kwargs.items()])
+        print("%s(%s) = %s" % (func.__name__, all_args, wrapped_result))
+        return wrapped_result
     return inner
 
 
-def add(a,b):
-    return a + b
+def add(x, y, a, b):
+    return x + y + a + b
+
+
+if __name__ == '__main__':
+    add = checker(add)
+    result = add(3, 4, b=5, a=10)
+    print("The returned result is: %d" % result)
