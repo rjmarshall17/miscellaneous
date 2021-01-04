9f74dc26e5d0b8dddd3e6c2ad7e097207ac0fb44
Rob Marshall <rob.marshall17@gmail.com>
2021-01-04 14:18:21 -0500

    Add prints and KMP to pseudocode

--- algo_export_knuth_morris_pratt.py
+++ algo_export_knuth_morris_pratt.py
@@ -2,13 +2,29 @@
 
 from typing import List
 
+
+def print_numbered_array(array):
+    l = len(array)
+    for i in range(l):
+        print('{:^5}'.format(str(i)), end='')
+    print()
+    for i in range(l):
+        print('{:^5}'.format(str(array[i])), end='')
+    print()
+    
 """
 Write a function that takes in two strings and checks if the first strings contains the
-second one using the Knuth-Morris-Pratt algorithm. The function should return a boolean
+second one using the Knuth-Morris-Pratt algorithm. The function should return a boolean.
+The Knuth-Morris-Pratt algorithm has a time complexity of O(n+m) where n is the length of
+the string to search and m is the length of the substring for which we're searching. The
+space complexity is O(m) since we need to create the pattern. 
 """
 
 def knuth_morris_pratt(string_in, substring_in):
+    # print_numbered_array(string_in)
     pattern = build_pattern(substring_in)
+    # print_numbered_array(substring_in)
+    # print_numbered_array(pattern)
     return does_match(string_in, substring_in, pattern)
 
 def build_pattern(substring_in):
@@ -17,13 +33,22 @@ def build_pattern(substring_in):
     i = 1
     while i < len(substring_in):
         if substring_in[i] == substring_in[j]:
+            # print("build_pattern: Found match: i=%d (%s) j=%d (%s)" % (i,
+            #                                                            substring_in[i],
+            #                                                            j,
+            #                                                            substring_in[j]))
             pattern[i] = j
+            # print("build_pattern: pattern[i (%d)] = j (%d)" % (i, j))
             i += 1
             j += 1
         elif j > 0:
+            # print("build_pattern: No match but j > 0, i.e. %d resetting to: pattern[j - 1] (%d) + 1 = %d" % (j,
+            #                                                                                                  pattern[j-1],
+            #                                                                                                  pattern[j-1]+1))
             j = pattern[j - 1] + 1
         else:
             i += 1
+            # print("build_pattern: No match and j (%d) is not greater than 0 i=%d" % (j,i))
     return pattern
 
 def does_match(string_in, substring_in, pattern):
@@ -31,15 +56,28 @@ def does_match(string_in, substring_in, pattern):
     j = 0
     while i + len(substring_in) - j <= len(string_in):
         if string_in[i] == substring_in[j]:
+            # print("does_match: string[i (%d)] %s == %s substring[j (%d)]" % (i,
+            #                                                                  string_in[i],
+            #                                                                  substring_in[j],
+            #                                                                  j))
             if j == len(substring_in) - 1:
+                # print("does_match: j (%d) == (%d) length of substring_in - 1" % (j,
+                #                                                                  len(substring_in)-1))
                 return True
             i += 1
             j += 1
+            # print("does_match: incremented i=%d j=%d" % (i,j))
         elif j > 0:
+            # print("does_match: No match found resetting j (%d) to pattern[j - 1 (%d)] (%d) to %d" % (j,
+            #                                                                                          j-1,
+            #                                                                                          pattern[j-1],
+            #                                                                                          pattern[j-1]+1))
             j = pattern[j - 1] + 1
         else:
+            # print("does_match: No match and j <= 0 incrementing i from %d to %d" % (i, i+1))
             i += 1
     return False
+
 
 EXAMPLE_INPUTS = [
     ["aefoaefcdaefcdaed", "aefcdaed"],
